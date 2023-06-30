92c3a4149de7b9f24a165bbd4cac7f6095b97b1d
alexandrav01 <93940491+alexandrav01@users.noreply.github.com>
2023-06-30 15:38:47 +0100

    Proceed to payment component
    Modal payment component with modal overlay pop-up.

--- order-summary/src/payment.jsx
+++ order-summary/src/payment.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
-import './payment.css'
+import "./payment.css";
 
-function Payment(){
-    return(
-        <>
-        <div className="button-wrap">
-        <button className="payment">Proceed to Payment</button>
-        <button className="cancel">Cancel Order</button>
+function Payment() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <div className="button-wrap">
+        <button className="payment" onClick={toggleModal}>
+          Proceed to Payment
+        </button>
+        {modal &&(
+            <div className="modal">
+          <div className="overlay" onClick={toggleModal}>
+            <div className="modal-content">
+              <h3>Thank you for your purchase</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button className="close-modal" onClick={toggleModal}>x</button>
+            </div>
+          </div>
         </div>
-        </>
-    )
+        )}
+
+        <button className="cancel">Cancel Order</button>
+      </div>
+    </>
+  );
 }
 
-export default Payment
+export default Payment;
